630d150844225cd662768cd0bf44ac8c3d1f217c
mitin20 <48494797+mitin20@users.noreply.github.com>
2020-06-21 07:26:07 -0500

    Update mvn.sh
    fix path

--- jenkins/test/mvn.sh
+++ jenkins/test/mvn.sh
@@ -3,7 +3,8 @@
 echo "***************************"
 echo "** Testing the code *******"
 echo "***************************"
-WORKSPACE=//home/jenkins/jenkins-resources/pipeline
 
+#WORKSPACE=//home/jenkins/jenkins-resources/pipeline
+WORKSPACE=/home/jenkins/jenkins-resources/jenkins_home/workspace/maven-pipeline
 
 docker run --rm  -v  $WORKSPACE/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine "$@"
