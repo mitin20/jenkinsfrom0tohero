bd2649dc94702f605ddb39dec2b11c3cb848df4a
mitin20 <48494797+mitin20@users.noreply.github.com>
2020-06-21 07:25:08 -0500

    Update mvn.sh
    fix path

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -4,6 +4,6 @@ echo "***************************"
 echo "** Building jar ***********"
 echo "***************************"
 
-#WORKSPACE=/home/jenkins/jenkins-data/jenkins_home/workspace/pipeline-docker-maven
-WORKSPACE=/home/jenkins/jenkins-resources/pipeline
+WORKSPACE=/home/jenkins/jenkins-resources/jenkins_home/workspace/maven-pipeline
+#WORKSPACE=/home/jenkins/jenkins-resources/pipeline
 docker run --rm  -v  $WORKSPACE/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine "$@"
